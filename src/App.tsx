a205c2b4b7867ed627ac057390a66c6c49bfcf6a
Micheal Callaghan <mehaul96@gmail.com>
2026-01-17 23:49:03 -0500

    update color and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
@@ -12,11 +12,14 @@ import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import Dashboard from "./pages/dashboard";
+import { Home } from "lucide-react";
+import { Layout } from "@/components/refine-ui/layout/layout.tsx";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,9 +32,22 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "fDBVkC-aLarQT-Fn1Y6r",
               }}
+              resources={[
+                {
+                  name: 'dashboard', 
+                  list: '/', 
+                  meta: { label: 'Home', icon: <Home /> }
+                }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                <Route element={
+                  <Layout>
+                    <Outlet />
+                  </Layout>
+                }>
+                  <Route path="/" element={<Dashboard />} />
+                </Route>
               </Routes>
               <Toaster />
               <RefineKbar />
