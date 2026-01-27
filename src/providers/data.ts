27556b226593bb510e6dc1e02059705f009a48f5
Micheal Callaghan <mehaul96@gmail.com>
2026-01-26 23:46:23 -0500

    add clone to response.json

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -25,7 +25,7 @@ const options: CreateDataProviderOptions = {
       return params;
     },
     mapResponse: async (response) => {
-      const payload: ListResponse = await response.json();
+      const payload: ListResponse = await response.clone().json();
 
       return payload.data ?? [];
     },
