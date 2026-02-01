1fc421efa3c3333742535072a269020e250260d8
Micheal Callaghan <mehaul96@gmail.com>
2026-02-01 00:51:44 -0500

    finish schema and begin create class component

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -1,6 +1,15 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb"
+import { CreateView } from "@/components/refine-ui/views/create-view"
+
 const create = () => {
   return (
-    <div>create</div>
+    <CreateView className="class-view"> 
+      <Breadcrumb />
+
+      <h1 className="page-title">Create a Class</h1>
+
+      <div></div>
+    </CreateView>
   )
 }
 
